a3a3d32db82e4feea5445f0c77e8875193911c31
Rajilatha <rajilatha1996@gmail.com>
2021-04-02 11:25:56 +0530

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'login';
+ email:any;
+ password:any;
+
+ loginUser(){
+   if(this.email== "admin@gmail.com" && this.password== "Admin"){
+     alert("Welcome to our Page")
+   }
+   else{
+     alert("Please check Your details.")
+   }
+ }
 }
